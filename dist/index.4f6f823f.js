d232a2e965c892de3fe40aaee6933d71a1c225c6
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-09-26 15:41:16 +0200

    package json scripts añadidos

---
+++ dist/index.4f6f823f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".burger"),t=document.querySelector(".nav");e.addEventListener("click",(()=>{t.classList.toggle("nav-visible"),e.classList.toggle("burger-close")}));
+//# sourceMappingURL=index.4f6f823f.js.map
