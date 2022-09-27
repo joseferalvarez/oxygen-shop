17ae10589e6e70860bc20b33185a4feda35b43f2
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-09-27 10:14:48 +0200

    scrollbar javascript añadido

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,18 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".nav");
+const scrollbar = document.querySelector(".scrollbar");
 
 burger.addEventListener("click", () => {
     menu.classList.toggle("nav-visible");
     burger.classList.toggle("burger-close");
+});
+
+window.addEventListener("scroll", () => {
+    let scrollTop = window.scrollY;
+    let docHeight = document.body.offsetHeight;
+    let winHeight = window.innerHeight;
+    let scrollPercent = Math.round(((scrollTop) / (docHeight - winHeight)) * 100);
+
+    console.log(scrollPercent);
+    scrollbar.style.width = scrollPercent + "%"
 });
