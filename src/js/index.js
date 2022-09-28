9861aad1e302cf43a2b45027015b2027ba5e1b4c
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-09-28 13:20:45 +0200

    popup terminado y js reestructurado

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,82 @@
-const burger = document.querySelector(".burger");
-const menu = document.querySelector(".nav");
 const scrollbar = document.querySelector(".scrollbar");
-const returnBtn = document.querySelector(".return-button");
 const sendBtn = document.querySelector(".send");
-const selectPrices = document.querySelector(".select__pricing");
 const popup = document.querySelector(".popup");
+const popupContainer = document.querySelector(".popup__container");
+const URL_USER = "https://jsonplaceholder.typicode.com/posts";
+const URL_CURRENCIES = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json";
+const BASIC_PRICE = 0;
+const PROFESSIONAL_PRICE = 25;
+const PREMIUM_PRICE = 60;
+const REGEXP_NAME = /^.{2,100}$/;
+const REGEXP_EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+/*Abre un popup que no ha sido cerrado en 5 segundos*/
 setTimeout(() => {
-    popup.classList.add("popup-visible");
+    if (!localStorage.getItem("popupState") && !sessionStorage.getItem("popupState")) {
+        popup.classList.add("popup-visible");
+        popupContainer.classList.add("popup__container-front");
+    }
 }, 5000)
 
-burger.addEventListener("click", () => {
-    menu.classList.toggle("nav-visible");
-    burger.classList.toggle("burger-close");
+/*Abre y cierra el menu al pinchar en la hamburguesa*/
+document.querySelector(".burger").addEventListener("click", (e) => {
+    document.querySelector(".nav").classList.toggle("nav-visible");
+    e.target.classList.toggle("burger-close");
 });
 
+/*Actualiza la barra de porcentaje y activa el popup al 25%*/
 window.addEventListener("scroll", () => {
-    let scrollTop = window.scrollY;
-    let docHeight = document.body.offsetHeight;
-    let winHeight = window.innerHeight;
-    let scrollPercent = Math.round(((scrollTop) / (docHeight - winHeight)) * 100);
+    let scrollPercent = Math.round(((window.scrollY) / (document.body.offsetHeight - window.innerHeight)) * 100);
 
+    setScrollPopup(scrollPercent);
     scrollbar.style.width = scrollPercent + "%"
 });
 
-returnBtn.addEventListener("click", () => {
+/*Cierra el popup en la X y actualiza el local y session storage*/
+document.querySelector(".popup__cross").addEventListener("click", () => {
+    popup.classList.remove("popup-visible");
+    popupContainer.classList.remove("popup__container-front");
+
+    localStorage.setItem("popupState", "1");
+    sessionStorage.setItem("popupState", "1");
+});
+
+/*Cierra el popup con ESC y actualiza el local y session storage*/
+window.addEventListener("keyup", (e) => {
+    if (e.key === "Escape") {
+        popup.classList.remove("popup-visible");
+        popupContainer.classList.remove("popup__container-front");
+    }
+    localStorage.setItem("popupState", "1");
+    sessionStorage.setItem("popupState", "1");
+});
+
+/*Cierra el popup clicando fuera y actualiza el local y session storage*/
+window.addEventListener("click", (e) => {
+    if (!popup.contains(e.target)) {
+        popup.classList.remove("popup-visible");
+        popupContainer.classList.remove("popup__container-front");
+        localStorage.setItem("popupState", "1");
+        sessionStorage.setItem("popupState", "1");
+    }
+});
+
+/*Envia los datos de email a un servidor*/
+document.querySelector(".popup__button").addEventListener("click", () => {
+    let email = document.querySelector(".popup__input");
+
+    if (REGEXP_EMAIL.test(email.value)) {
+        sendForm(email, "", URL_USER)
+        popup.classList.remove("popup-visible");
+        localStorage.setItem("popupState", "1");
+        sessionStorage.setItem("popupState", "1");
+    } else {
+        email.classList.add("popup-error");
+    }
+});
+
+/*Vuelve al inicio de la pagina al pulsar el boton*/
+const returnBtn = document.querySelector(".return-button").addEventListener("click", () => {
     setTimeout(() => {
         window.scroll({
             top: 0,
@@ -33,55 +85,59 @@ returnBtn.addEventListener("click", () => {
     }, 200);
 });
 
+/* TODO: optimizar esto*/
+/*Envia los datos del formulario a un servidor*/
 sendBtn.addEventListener("click", () => {
     let name = document.querySelector(".contact__input-name");
     let email = document.querySelector(".contact__input-email");
     let check = document.querySelector(".contact__policies-checkbox");
-    const url = "https://jsonplaceholder.typicode.com/posts";
-    const regexpName = /^.{2,100}$/;
-    const regexpEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (!regexpName.test(name.value)) {
-        name.classList.add("contact__input-bad");
+    if (!REGEXP_NAME.test(name.value)) {
+        name.classList.add("input-error");
     } else {
-        name.classList.remove("contact__input-bad");
+        name.classList.remove("input-error");
     }
-    if (!regexpEmail.test(email.value)) {
-        email.classList.add("contact__input-bad");
+    if (!REGEXP_EMAIL.test(email.value)) {
+        email.classList.add("input-error");
     } else {
-        email.classList.remove("contact__input-bad");
+        email.classList.remove("input-error");
     }
 
-    if (regexpName.test(name.value) && regexpEmail.test(email.value) && check.checked) {
-        sendForm(name.value, email.value, url);
+    if (REGEXP_NAME.test(name.value) && REGEXP_EMAIL.test(email.value) && check.checked) {
+        sendForm(name.value, email.value, URL_USER);
     }
-
 });
 
-selectPrices.addEventListener("change", () => {
-    let currencie = (selectPrices.value).toLowerCase();
-    const URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json"
-    let cardProfessional = document.querySelector(".card__price-professional")
+/*Cambia las divisas de monedas*/
+document.querySelector(".select__pricing").addEventListener("change", (e) => {
+    let currencie = (e.target.value).toLowerCase();
+    let cardBasic = (document.querySelector(".card__price-basic"));
+    let cardProfessional = document.querySelector(".card__price-professional");
     let cardPremium = document.querySelector(".card__price-premium");
 
-    const promise = fetch(URL)
+    const promise = fetch(URL_CURRENCIES)
         .then(response => response.json())
         .then(data => {
             if (currencie === "eur") {
-                cardProfessional.innerText = "$" + (data.usd.eur * 25).toFixed(2);
-                cardPremium.innerText = "$" + (data.usd.eur * 60).toFixed(2);
+                cardBasic.innerText = "€" + ((data.usd.eur * BASIC_PRICE).toFixed(2));
+                cardProfessional.innerText = "€" + (data.usd.eur * PROFESSIONAL_PRICE).toFixed(2);
+                cardPremium.innerText = "€" + (data.usd.eur * PREMIUM_PRICE).toFixed(2);
             } else if (currencie === "gbp") {
-                cardProfessional.innerText = "$" + (data.usd.gbp * 25).toFixed(2);
-                cardPremium.innerText = "$" + (data.usd.gbp * 60).toFixed(2);
+                cardBasic.innerText = "£" + ((data.usd.eur * BASIC_PRICE).toFixed(2));
+                cardProfessional.innerText = "£" + (data.usd.gbp * PROFESSIONAL_PRICE).toFixed(2);
+                cardPremium.innerText = "£" + (data.usd.gbp * PREMIUM_PRICE).toFixed(2);
             } else {
-                cardProfessional.innerText = "$25";
-                cardPremium.innerText = "$60";
+                cardBasic.innerText = "$" + BASIC_PRICE;
+                cardProfessional.innerText = "$" + PROFESSIONAL_PRICE;
+                cardPremium.innerText = "$" + PREMIUM_PRICE;
             }
         });
 });
 
-function sendForm(email, name, url) {
-    fetch(url, {
+
+/*Envia al servidor el email y nombre de contacto*/
+async function sendForm(email, name, url) {
+    await fetch(url, {
         method: "POST",
         body: JSON.stringify({
             email: email,
@@ -91,6 +147,13 @@ function sendForm(email, name, url) {
             'Content-type': 'application/json; charset=UTF-8',
         },
     })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+        .then((response) => response.json());
+}
+
+/*Muestra el popup cuando la pagina este al 25%*/
+function setScrollPopup(percent) {
+    if (percent === 25 && !localStorage.getItem("popupState")) {
+        popup.classList.add("popup-visible");
+        popupContainer.classList.add("popup__container-front");
+    }
 }
